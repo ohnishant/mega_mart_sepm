1efa095b9394ef984267f4af08b7850bd954a22d
ohnishant <nishant.mishra550@gmail.com>
2023-03-24 12:05:29 +0530

    Initial Commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+
+class App(Tk):
+    def __init__(
+        self,
+        screenName: str | None = None,
+        baseName: str | None = None,
+        className: str = "Tk",
+        useTk: bool = True,
+        sync: bool = False,
+        use: str | None = None,
+    ) -> None:
+        super().__init__(screenName, baseName, className, useTk, sync, use)
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
