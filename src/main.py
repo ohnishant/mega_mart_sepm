d5b286031a09cbc597fc5451fe5b83274c8415c8
ohnishant <nishant.mishra550@gmail.com>
2023-04-04 22:26:57 +0530

    feat: barebones login frame

--- src/main.py
+++ src/main.py
@@ -5,16 +5,20 @@ import screens.login
 
 
 class App(ctk.CTk):
-    def __init__(self, fg_color: Optional[Union[str, Tuple[str, str]]] = None, **kwargs):
+    def __init__(
+        self, fg_color: Optional[Union[str, Tuple[str, str]]] = None, **kwargs
+    ):
         super().__init__(fg_color, **kwargs)
         self.geometry("1366x768")
         self.draw_login()
-    
+
     def draw_login(self):
-        loginFrame = screens.login.Login(self)
+        loginFrame = screens.login.Login(self, bg_color="Green")
         loginFrame.pack()
 
 
 if __name__ == "__main__":
     app = App()
+    app.columnconfigure(0, weight=1)
+    app.rowconfigure(0, weight=1)
     app.mainloop()
--- src/screens/login.py
+++ src/screens/login.py
@@ -1,4 +1,8 @@
+import os
+from tkinter import NSEW
 from typing import NoReturn, Optional, Tuple, Union
+
+from PIL import Image
 
 import customtkinter as ctk
 
@@ -6,9 +10,9 @@ import customtkinter as ctk
 class Login(ctk.CTkFrame):
     def __init__(
         self,
-        master: any,
-        width: int = 200,
-        height: int = 200,
+        master: any, # type: ignore
+        width: int = 500,
+        height: int = 500,
         corner_radius: Optional[Union[int, str]] = None,
         border_width: Optional[Union[int, str]] = None,
         bg_color: Union[str, Tuple[str, str]] = "transparent",
@@ -18,7 +22,7 @@ class Login(ctk.CTkFrame):
             Tuple[Union[str, Tuple[str, str]]], None
         ] = None,
         overwrite_preferred_drawing_method: Union[str, None] = None,
-        **kwargs
+        **kwargs,
     ):
         super().__init__(
             master,
@@ -31,24 +35,42 @@ class Login(ctk.CTkFrame):
             border_color,
             background_corner_colors,
             overwrite_preferred_drawing_method,
-            **kwargs
+            **kwargs,
         )
 
+        image_path = os.path.join(os.path.dirname(os.getcwd()), "media")
+        # self.logo_image = ctk.CTkImage(Image.open(os.path.join(image_path, "mega_mart_logo.png")), size=("150x150"))
+
+        # self.bind("<F11>", command=self.toggle_fullscreen)
         self.draw()
 
     def draw(self):
-        fullscreen_button = ctk.CTkButton(self, text="Toggle Fullscreen", command=self.toggle_fullscreen)
-        fullscreen_button.pack()
+        # setup grid
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
 
+        # create logo frame
+        self.logo_frame: ctk.CTkFrame = ctk.CTkFrame(self, corner_radius=0, width=500, bg_color="White")
+        self.logo_image: ctk.CTkImage = ctk.CTkImage(Image.open(".//media//mega_mart_logo.png"), size=(500,500))
+        self.logo_label: ctk.CTkLabel = ctk.CTkLabel(self, image=self.logo_image, text="")
+        self.logo_label.grid(row = 0, column = 0, sticky=NSEW)
+
+        # create credential frame
+        self.credential_frame: ctk.CTkFrame = ctk.CTkFrame(self, bg_color="transparent", corner_radius=0,)
+        self.credential_frame.grid(row = 0, column = 1, sticky = NSEW)
+        
 
     fullscreen: bool = False
-    def toggle_fullscreen(self) -> NoReturn:
-        self.fullscreen = not(self.fullscreen)
-        self.master.attributes('-fullscreen', self.fullscreen)
+
+    def toggle_fullscreen(self) -> None:
+        self.fullscreen = not (self.fullscreen)
+        self.master.attributes("-fullscreen", self.fullscreen) # type: ignore
+
 
 if __name__ == "__main__":
     app = ctk.CTk()
     app.geometry(f"{1366}x{768}")
-    Login(app).pack()
+    app.rowconfigure(0, weight=1)
+    app.columnconfigure(0, weight=1)
+    Login(app).pack(fill="both")
     app.mainloop()
-
